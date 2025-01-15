e7fada8ac80b543eb514673aff10083ab9250bed
Andrew Garner <andrew@andrewgarner.com>
2025-01-15 17:18:40 +0000

    Import existing root organisation

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
 provider "aws" {
   region = var.aws_region
 }
+
+import {
+  to = aws_organizations_organization.root
+  id = "o-jsxmqn3coi"
+}
+
+resource "aws_organizations_organization" "root" {
+  aws_service_access_principals = ["sso.amazonaws.com"]
+}
