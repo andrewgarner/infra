049d3b7237af9e0a714278363d20743a20f74522
Andrew Garner <andrew@andrewgarner.com>
2025-01-15 17:31:15 +0000

    Import existing scratch organisation unit

--- main.tf
+++ main.tf
@@ -10,3 +10,13 @@ import {
 resource "aws_organizations_organization" "root" {
   aws_service_access_principals = ["sso.amazonaws.com"]
 }
+
+import {
+  to = aws_organizations_organizational_unit.scratch
+  id = "ou-wnve-948lv2iq"
+}
+
+resource "aws_organizations_organizational_unit" "scratch" {
+  name      = "Scratch"
+  parent_id = aws_organizations_organization.root.roots.0.id
+}
